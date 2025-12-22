3da5f7e6a16800d5f9f5a29c6c1b3ec02d9e0d83
EugenePrygaro <eugeneprygaro@gmail.com>
2025-12-22 18:25:29 +0200

    about section

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter  as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Tours from './components/Tours';
-import About from './components/About';
 import Auth from './components/Auth';
 import Register from './components/Register';
 import Cab from './components/Cab';
@@ -13,7 +12,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/tours" element={<Tours />} />
-        <Route path="/about" element={<About />} />
         <Route path="/auth" element={<Auth />} />
         <Route path="/register" element={<Register />} />
         <Route path="/cab" element={<Cab />} />
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,6 +2,8 @@ import { Link } from "react-router-dom";
 import NavBar from './NavBar.js';
 import '../index.css'
 import home_icons from './styles/images/icons/home-icons.svg';
+import teamImg from './styles/images/about-team.jpg';
+import templeImg from './styles/images/about-temple.jpg'
 
 function Home() {
   return (
@@ -45,6 +47,98 @@ function Home() {
               </li>
             </ul>
           </div>
+        </section>
+      <section className="home-about-section">
+        <div className="home-container home-about-container">
+          <div className="about-content">
+            <div className="about-row">
+              <div className="about-row-first-wrapper">
+                <p className="about-description">
+                    <span className="accent-red">TravelLux</span> is not just a travel agency, it is your personal guide to the world of unforgettable adventures and luxury travel.
+                      We believe that every journey is a unique story and our mission is to make your story special.
+                      Since our inception, we have strived to create unparalleled travel  xperiences,
+                      exceeding the expectations of the most demanding customers
+                </p>
+                <button className="about-contact-btn">Contact Us</button>
+              </div>
+                
+                <img src={templeImg} alt="Ancient Temple" width="400px" className="about-img temple"/>
+            </div>
+            <div className="about-row reverse">
+                  <img src={teamImg} alt="Our Team" width="400px" className="about-img team"/>
+                <p className="about-description">
+                  <span className="accent-red">Our team</span> is a collection of seasoned travelers, tourism experts and passionate researchers.
+                    We personally visit every destination we offer to ensure you get only the best.
+                </p>
+              </div>
+            </div>
+        </div>
+        </section>
+      <section className="home-contacts-section">
+        <div className="home-container home-contacts-container">
+          <div className="contacts-line"></div>
+          <div className="contacs-content">
+            <h2 className="contacts-header">Contacts</h2>
+            <h3 className="contacts-pre-header">Ready to plan your next premium flight?</h3>
+            <p className="contacts-text">Leave your contact details in the form below, and our dedicated manager from Travel Lux will call you back shortly to assist with your booking, provide a tailored quote, or answer any questions about our aviation services.
+              <br></br><br></br>
+              We are available from Monday to Friday, 09:00 – 18:00
+            </p>
+            <div className="form-container">
+              <div className="input-field-container">
+                <div className="input-group">
+                  <label htmlFor="name">Full name</label>
+                  <div className="input-wrapper">
+                    <span className="icon">👤</span>
+                    <input 
+                      type="text" 
+                      id="name" 
+                      placeholder="Enter your name" 
+                      className="contact-input"
+                    />
+                  </div>
+                </div>
+                <div className="input-group">
+                  <label htmlFor="name">Email</label>
+                  <div className="input-wrapper">
+                    <span className="icon"></span>
+                    <input 
+                      type="email" 
+                      id="email" 
+                      placeholder="Enter your email" 
+                      className="contact-input"
+                    />
+                  </div>
+                </div>
+                <div className="input-group">
+                  <label htmlFor="phone">Phone number</label>
+                  <div className="input-wrapper">
+                    <span className="icon">📞</span>
+                    <input 
+                      type="tel" 
+                      id="phone" 
+                      placeholder="+380 (__) ___-__-__" 
+                      className="contact-input"
+                    />
+                  </div>
+                <div className="input-group">
+                  <label htmlFor="time">Preferred time for call</label>
+                  <div className="input-wrapper">
+                    <span className="icon">⏰</span>
+                    <select id="time" className="contact-input select-input">
+                      <option value="morning">Morning (09:00 - 12:00)</option>
+                      <option value="afternoon">Afternoon (12:00 - 15:00)</option>
+                      <option value="evening">Evening (15:00 - 18:00)</option>
+                    </select>
+                    </div>
+                    <button className="input-field-button">Request a Callback</button>
+                  </div>
+                </div>
+              </div>
+              <p className="input-field-text">Travel Lux — elevating your journey to new heights.</p>
+            </div>
+          </div>
+        </div>
         </section>
         <footer></footer>
     </main>
